3c9d135138a0babc36b78330e8a7970a3f34adb7
Adarsh <pandey.adarshking@gmail.com>
2023-07-17 13:33:55 +0530

    dev- comleeted todo application

--- main.js
+++ main.js
@@ -28,3 +28,33 @@ function createTaskItem(id, task, description) {
 
     return listItem;
 }
+
+// Function to add a new task
+function addTask() {
+    const task = taskInput.value.trim();
+    const description = descriptionInput.value.trim();
+
+    if (task === '') {
+        alert('Please enter a task');
+        return;
+    }
+
+    const taskItem = createTaskItem(taskId, task, description);
+    taskList.appendChild(taskItem);
+
+    // Increment the task ID for the next task
+    taskId++;
+
+    // Clear the input fields
+    taskInput.value = '';
+    descriptionInput.value = '';
+}
+
+// Function to delete a task
+function deleteTask(event) {
+    const listItem = event.target.parentNode;
+    listItem.remove();
+}
+
+// Event listener for the "Add Task" button click
+addTaskBtn.addEventListener('click', addTask);
