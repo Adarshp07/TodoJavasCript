c6410953dfce543faea4eb1955bc92bd28aa8850
Adarsh <pandey.adarshking@gmail.com>
2023-07-21 16:40:47 +0530

    Dev- Final development product completed

--- NodeTOdo/todo.js
+++ NodeTOdo/todo.js
@@ -2,9 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
-app.use(bodyParser).json();
+app.use(bodyParser.json());
 let todos = [];
+
+function removeAtIndex(arr, index) {
+    let newArray = [];
+    for (let i = 0; i < newArray.length; i++) {
+        if (i !== index) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
 function FindIndex(arr, id) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].id === id) {
@@ -26,15 +36,38 @@ app.get('/todos', (req, res) => {
 
 //create a todo
 var c = 1;
-app.post('/post', (req, res) => {
+app.post('/todos', (req, res) => {
     const newtodo = {
         id: c,
         title: req.body.title,
         description: req.body.description
     };
     c = c + 1;
-    todos.push(newTodo);
-    req.status(201).json(newTodo);
+    todos.push(newtodo);
+    res.status(201).json(newtodo);
 
 });
 
+//delete
+
+app.delete('/todos/:id', (req, res) => {
+    const todoIndex = FindIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+        res.status(404).send();
+    }
+    else {
+        todos = removeAtIndex(todos, todoIndex);
+        res.status(200).send();
+    }
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+    res.status(404).send();
+});
+
+module.exports = app;
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
