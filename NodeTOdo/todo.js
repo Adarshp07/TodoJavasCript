10f37d614e04183c4ac251898f169677b7739b67
Adarsh <pandey.adarshking@gmail.com>
2023-07-24 16:51:35 +0530

    added- index.html and connected frontend and backend

--- NodeTOdo/todo.js
+++ NodeTOdo/todo.js
@@ -1,70 +1,84 @@
+const path = require('node:path');
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(bodyParser.json());
+
 let todos = [];
 
-function removeAtIndex(arr, index) {
-    let newArray = [];
-    for (let i = 0; i < newArray.length; i++) {
-        if (i !== index) {
-            newArray.push(arr[i]);
-        }
-    }
-    return newArray;
-}
-
-function FindIndex(arr, id) {
+function findIndex(arr, id) {
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id === id) {
-            return i
-        }
+        if (arr[i].id === id) return i;
     }
     return -1;
 }
 
-
-
-
-// get element
+function removeAtIndex(arr, index) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== index) newArray.push(arr[i]);
+    }
+    return newArray;
+}
 
 app.get('/todos', (req, res) => {
     res.json(todos);
-})
+});
 
+app.get('/todos/:id', (req, res) => {
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+        res.status(404).send();
+    } else {
+        res.json(todos[todoIndex]);
+    }
+});
 
-//create a todo
-var c = 1;
 app.post('/todos', (req, res) => {
-    const newtodo = {
-        id: c,
+    const newTodo = {
+        id: Math.floor(Math.random() * 1000000), // unique random id
         title: req.body.title,
         description: req.body.description
     };
-    c = c + 1;
-    todos.push(newtodo);
-    res.status(201).json(newtodo);
-
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
 });
 
-//delete
-
-app.delete('/todos/:id', (req, res) => {
-    const todoIndex = FindIndex(todos, parseInt(req.params.id));
+app.put('/todos/:id', (req, res) => {
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
     if (todoIndex === -1) {
         res.status(404).send();
+    } else {
+        todos[todoIndex].title = req.body.title;
+        todos[todoIndex].description = req.body.description;
+        res.json(todos[todoIndex]);
     }
-    else {
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+        res.status(404).send();
+    } else {
         todos = removeAtIndex(todos, todoIndex);
         res.status(200).send();
     }
 });
 
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
 // for all other routes, return 404
 app.use((req, res, next) => {
     res.status(404).send();
 });
+
+
 
 module.exports = app;
 const port = 3000;
